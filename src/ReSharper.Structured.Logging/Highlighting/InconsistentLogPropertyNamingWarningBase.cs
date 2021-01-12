b656d4dddcfa91abd525ee84f7277d9a303aeec8
Oleg Shevchenko <olsh.me@gmail.com>
2021-01-12 21:41:14 +0300

    Fix inconsistent naming tooltip. Closes #17

--- src/ReSharper.Structured.Logging/Highlighting/InconsistentLogPropertyNamingWarningBase.cs
+++ src/ReSharper.Structured.Logging/Highlighting/InconsistentLogPropertyNamingWarningBase.cs
@@ -2,11 +2,11 @@ namespace ReSharper.Structured.Logging.Highlighting
 {
     public class InconsistentLogPropertyNamingWarningBase
     {
-        protected const string Message = "Property name does not match naming rules.";
+        protected const string Message = "Property name '{0}' does not match naming rules. Suggested name is '{1}'.";
 
         protected string GetToolTipMessage(string propertyName, string suggestedName)
         {
-            return $"Property name '{propertyName}' does not match naming rules'. Suggested name is '{suggestedName}'.";
+            return $"Property name '{propertyName}' does not match naming rules. Suggested name is '{suggestedName}'.";
         }
     }
 }
