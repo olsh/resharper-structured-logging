35aa59512217c01240869e9141c48957ba687873
Oleg <olsh.me@gmail.com>
2023-08-03 18:08:57 +0300

    Add plugin bundle #95

--- src/rider/main/kotlin/com/jetbrains/rider/settings/StructuredLoggingPluginOptionsPage.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/settings/StructuredLoggingPluginOptionsPage.kt
@@ -1,8 +1,12 @@
 package com.jetbrains.rider.settings
 
 import com.jetbrains.rider.settings.simple.SimpleOptionsPage
+import com.jetbrains.rider.settings.StructuredLoggingBundle
 
-class StructuredLoggingPluginOptionsPage : SimpleOptionsPage("Structured Logging", "StructuredLogging") {
+class CognitiveComplexityOptionsPage : SimpleOptionsPage(
+    name = StructuredLoggingBundle.message("configurable.name.structuredlogging.title"),
+    pageId = "StructuredLogging")
+{
     override fun getId(): String {
         return "StructuredLogging"
     }
